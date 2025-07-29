f39d175e7bfc85c32be857ccec731e18409ae525
axoo01 <juniorax03@gmail.com>
2025-07-29 22:46:48 +0200

    Mobile first hero

--- nikebody/src/sections/Hero.jsx
+++ nikebody/src/sections/Hero.jsx
@@ -43,10 +43,10 @@ const Hero = () => {
         className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container z-10"
       >
         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-          <p className="text-xl font-montserrat text-coral-red pt-2">
+          <p className="text-xl font-montserrat text-coral-red">
             Our Summer Collection
           </p>
-          <h1 className="mt-6 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <h1 className="max-sm:mt-6 mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
             <span className="xl:bg-white/70 dark:xl:bg-dark-primary/70 xl:whitespace-nowrap relative z-10 pr-10">
               The New Arrival
             </span>
@@ -56,12 +56,12 @@ const Hero = () => {
             </span>
             Shoes
           </h1>
-          <p className="font-montserrat text-slate-gray dark:text-light-gray text-lg max-sm:text-[16px] leading-8 my-6 sm:max-w-sm">
+          <p className="font-montserrat text-slate-gray dark:text-light-gray text-lg max-sm:text-[16px] leading-8 mt-6 sm:mb-14 max-sm:mb-6 sm:max-w-sm">
             Discover stylish Nike arrivals, quality comfort, and innovation for
             your active life.
           </p>
           <Button label="Shop now" iconURL={arrowRight} />
-          <div className="flex justify-start items-start flex-wrap mt-8 w-full gap-16 max-sm:gap-4">
+          <div className="flex justify-start items-start flex-wrap mt-20 max-sm:mt-8 w-full gap-16 max-sm:gap-4">
             {statistics.map((stat, index) => (
               <div key={stat.label}>
                 <p className="text-4xl font-palanquin font-bold">
