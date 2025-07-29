567b23e11e9b8b0f96665505ff940ce0fa34f09f
axoo01 <juniorax03@gmail.com>
2025-07-29 18:03:50 +0200

    Mobile first (Hero section)

--- nikebody/src/sections/Hero.jsx
+++ nikebody/src/sections/Hero.jsx
@@ -43,25 +43,25 @@ const Hero = () => {
         className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container z-10"
       >
         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-          <p className="text-xl font-montserrat text-coral-red py-2">
+          <p className="text-xl font-montserrat text-coral-red pt-2">
             Our Summer Collection
           </p>
-          <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <h1 className="mt-6 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
             <span className="xl:bg-white/70 dark:xl:bg-dark-primary/70 xl:whitespace-nowrap relative z-10 pr-10">
               The New Arrival
             </span>
             <br />
-            <span className="text-coral-red mt-3 inline-block">
+            <span className="text-coral-red mt-2 inline-block">
               Nike
             </span>
             Shoes
           </h1>
-          <p className="font-montserrat text-slate-gray dark:text-light-gray text-lg leading-8 mt-6 mb-6 sm:max-w-sm">
+          <p className="font-montserrat text-slate-gray dark:text-light-gray text-lg max-sm:text-[16px] leading-8 my-6 sm:max-w-sm">
             Discover stylish Nike arrivals, quality comfort, and innovation for
             your active life.
           </p>
           <Button label="Shop now" iconURL={arrowRight} />
-          <div className="flex justify-start items-start flex-wrap mt-14 w-full gap-16 max-sm:gap-10">
+          <div className="flex justify-start items-start flex-wrap mt-8 w-full gap-16 max-sm:gap-6">
             {statistics.map((stat, index) => (
               <div key={stat.label}>
                 <p className="text-4xl font-palanquin font-bold">
